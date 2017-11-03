6ff2776ba4e057546ba8ca69607879eacf743733
Allen Keef <allen.keef@colorado.edu>
2017-11-02 20:50:33 -0600

    Added Milestone 3 Files

--- backend/websiteDB.sql
+++ backend/websiteDB.sql
@@ -1,41 +1,69 @@
-CREATE DATABASE IF NOT EXISTS `websiteDB`;
+DROP DATABASE IF EXISTS websiteDB;
 
-USE `websiteDB`;
+CREATE DATABASE websiteDB;
 
-/*Table structure for table `Customers` */
+USE websiteDB;
 
-DROP TABLE IF EXISTS `Customers`;
+/*Table structure for table customers */
 
-CREATE TABLE `Customers` (
-  `customerId` int(11) NOT NULL AUTO_INCREMENT,
-  `firstName` varchar(20) DEFAULT NULL,
-  `lastName` varchar(20) DEFAULT NULL,
-  `phoneNumber` varchar(15) DEFAULT NULL,
-  `emailAddress` varchar(50) DEFAULT NULL,
-  `city` varchar(20) DEFAULT NULL,
-  PRIMARY KEY (`customerId`)
+DROP TABLE IF EXISTS customers;
+
+CREATE TABLE customers (
+  customerId int(11) NOT NULL AUTO_INCREMENT,
+  firstName varchar(20) DEFAULT NULL,
+  lastName varchar(20) DEFAULT NULL,
+  emailAddress varchar(50) DEFAULT NULL,
+  phoneNumber varchar(12) DEFAULT NULL,
+  city varchar(20) DEFAULT NULL,
+  PRIMARY KEY (customerId)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
-/*Data for the table `Customers` */
+/*Data for the table customers */
 
-INSERT INTO Customers VALUES(0, 'Test', 'User', 3031231234, 'test@test.com', 'Boulder');
+INSERT INTO customers VALUES
+(72391,'Alex','Delgado','alexdel413@gmail.com','720-242-7210','Northglenn'),
+(72324,'Hector','Marin','HectorMar425@gmail.com','303-808-3003','Littleton'),
+(72384,'Allen','Reyes','Allen424@gmail.com','720-242-6126','Boulder'),
+(0,'Test','User','Test@gmail.com','555-555-5555','Boulder');
 
-/*Table structure for table `Orders` */
+/*Table structure for table orders */
 
-DROP TABLE IF EXISTS `Orders`;
+DROP TABLE IF EXISTS orders;
 
-CREATE TABLE `Orders` (
-  `orderId` int(11) NOT NULL AUTO_INCREMENT,
-  `address` varchar(50) DEFAULT NULL,
-  `city` varchar(20) DEFAULT NULL,
-  `orderDate` DATETIME DEFAULT NULL,
-  `completion` tinyint(1) DEFAULT NULL,
-  `customerId` int(11) DEFAULT NULL,
-  PRIMARY KEY (`orderId`),
-  KEY `customerId` (`customerId`),
-  CONSTRAINT `Orders_ibfk_1` FOREIGN KEY (`customerId`) REFERENCES `Customers` (`customerId`)
+CREATE TABLE orders (
+  orderId int(11) NOT NULL AUTO_INCREMENT,
+  address varchar(50) DEFAULT NULL,
+  city varchar(20) DEFAULT NULL,
+  orderDate DATE DEFAULT NULL,
+  description varchar(500) DEFAULT NULL,
+  status varchar(10) DEFAULT NULL,
+  customerId int(11) DEFAULT NULL,
+  PRIMARY KEY (orderId),
+  KEY customerId (customerId),
+  CONSTRAINT orders_ibfk_1 FOREIGN KEY (customerId) REFERENCES customers (customerId)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
-/*Data for the table `Orders` */
+/*Data for the table orders */
 
-INSERT INTO Orders VALUES(0, 'Test Address', 'Boulder', NOW(), 0, 1);
+INSERT INTO orders VALUES
+(1151423,'10431 Livingston Dr','Northglenn','2017-04-13','Sidewalk Replacement','Complete',72391),
+(1234123,'11355 Highline Dr','Littleton','2017-08-20','Cement Patio','Initial',72324),
+(5657475,'820 28th st','Boulder','2017-10-20','Driveway Replacement','Ongoing',72384),
+(0,'Test Address','Boulder',CURDATE(),'Test Description','Initial',72392);
+
+/*Table structure for table users */
+
+DROP TABLE IF EXISTS users;
+
+CREATE TABLE users (
+  userId int(11) NOT NULL AUTO_INCREMENT,
+  userType varchar(50) DEFAULT NULL,
+  username varchar(100) NOT NULL,
+  password varchar(100) NOT NULL,
+  PRIMARY KEY (userId)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+/*Data for the table users */
+
+INSERT INTO users VALUES
+(0,'Admin','Admin Username','Encrypted Password');
