cb783b767c4b6e0def5f08f853b3142f91b18541
Tanner Quigley <TannerQuigley@me.com>
2017-11-08 13:06:05 -0700

    Created routes in app

--- app.py
+++ app.py
@@ -3,3 +3,19 @@ app = Flask(__name__)
 @app.route("/")
 def main():
     return render_template('index.html')
+
+@app.route("/services")
+def services():
+    return render_template('services.html')
+
+@app.route("/contact")
+def contact():
+    return render_template('contact.html')
+
+@app.route("/projects")
+def projects():
+    return render_template('project.html')
+
+@app.route("/info")
+def info():
+    return render_template('info.html')
