6bfb0c1974da0161d12c1992b81b55bdfefa1c52
Tanner Quigley <TannerQuigley@me.com>
2017-11-08 13:14:37 -0700

    updated links

--- app.py
+++ app.py
@@ -1,5 +1,8 @@
 from flask import Flask, render_template
 app = Flask(__name__)
+
+
+
 @app.route("/")
 def main():
     return render_template('index.html')
@@ -19,3 +22,9 @@ def projects():
 @app.route("/info")
 def info():
     return render_template('info.html')
+
+@app.route('/contact',methods=['POST'])
+def submitContact():
+    _firstname = request.form['firstname']
+    _lastname = request.form['lastname']
+    _message = request.form['Message']
