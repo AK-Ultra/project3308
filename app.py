fa3df752e325e853401fbf3457a1a3bad7891db5
Tanner Quigley <TannerQuigley@me.com>
2017-12-06 13:40:55 -0700

    small design change to nav bar

--- app.py
+++ app.py
@@ -1,24 +1,24 @@
 from flask import Flask, render_template, flash, request, url_for , redirect, session
-import MySQLdb
+# import MySQLdb
 app = Flask(__name__)
-#allows the hardcoded login logic to work before sql
-app.secret_key = 'password'
-# Command to Run: FLASK_APP=app.py flask run
-
-# Database Connection
-db = MySQLdb.connect(host='localhost',user='root',passwd='539',db='websiteDB')
-
-# Query orders
-cursor = db.cursor()
-cursor.execute('SELECT * FROM orders;')
-projectData = cursor.fetchall()
-cursor.close()
-
-# Query reviews
-cursor = db.cursor()
-cursor.execute('SELECT t1.description, t3.firstName, LEFT(t3.lastName,1), t1.starCount FROM reviews t1 INNER JOIN orders t2 ON t1.orderID = t2.orderID INNER JOIN customers t3 ON t2.customerID = t3.customerID;')
-reviewData = cursor.fetchall()
-cursor.close()
+# #allows the hardcoded login logic to work before sql
+# app.secret_key = 'password'
+# # Command to Run: FLASK_APP=app.py flask run
+#
+# # Database Connection
+# db = MySQLdb.connect(host='localhost',user='root',passwd='539',db='websiteDB')
+#
+# # Query orders
+# cursor = db.cursor()
+# cursor.execute('SELECT * FROM orders;')
+# projectData = cursor.fetchall()
+# cursor.close()
+#
+# # Query reviews
+# cursor = db.cursor()
+# cursor.execute('SELECT t1.description, t3.firstName, LEFT(t3.lastName,1), t1.starCount FROM reviews t1 INNER JOIN orders t2 ON t1.orderID = t2.orderID INNER JOIN customers t3 ON t2.customerID = t3.customerID;')
+# reviewData = cursor.fetchall()
+# cursor.close()
 
 @app.route("/")
 def main():
@@ -44,7 +44,7 @@ def info():
 def submitContact():
     _firstname = request.form['firstname']
     _lastname = request.form['lastname']
-    _message = request.form['Message']    
+    _message = request.form['Message']
 
 @app.route("/reviews")
 def reviews():
@@ -54,20 +54,19 @@ def reviews():
 def login():
      error = None
      try:
-            #They have submitted a login 
+            #They have submitted a login
          if request.method == "POST":
               attempted_username = request.form['username']
               attempted_password = request.form['password']
               flash(attempted_username)
               flash(attempted_password)
-              #Basic for debugging  Sql imp here.          
+              #Basic for debugging  Sql imp here.
               if attempted_username == "admin" and attempted_password == "password":
-                 #Then we redirect to projects which will be admin only 
+                 #Then we redirect to projects which will be admin only
                   return redirect(url_for('projects'))
               else:
-                  error = "Invalid Try Again" 
+                  error = "Invalid Try Again"
          return render_template('login.html',error = error)
      except Exception as e:
          flash(e)
          return render_template('login.html',error = error)
-
